5a839f043d57b52c48dbc6798c8898bf35c98996
SnakySeek <134644626+SnakySeek@users.noreply.github.com>
2023-07-04 10:19:02 -0600

    add baseline

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const Axios = require("axios");
 const fs = require('fs');
+const parser = require('htmlparser2')
+
+async function downloadFile(url) {
+    const { data, headers } = await Axios({
+        url: url,
+        method: 'GET',
+        responseType: 'stream',
+    });
+    const stream = fs.createWriteStream('Clip.mp4');
+    data.pipe(stream);
+}
+
+async function download(downloadUrl) {
+    if (String(downloadUrl).includes('cdn.medal.tv/ugcc/content-social')) {
+        downloadFile(downloadUrl)
+    } else {
+
+    }
+
+}
+download('')
