35f872a5781a5c7f4cc42be0aea5e8add87571d9
SnakySeek <134644626+SnakySeek@users.noreply.github.com>
2023-07-04 12:07:39 -0600

    geez finally

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 const Axios = require("axios");
 const fs = require('fs');
+const { parse } = require('node-html-parser');
 
 
 
-
-async function getData(url) {
-    const { data, headers } = await Axios({
-        url: String(url),
-        method: 'GET',
-        responseType: 'stream',
-    });
-    return data;
+async function downloadFile(url, needsHtml) {
+    if (needsHtml) {
+        Axios.get(url).then(response => {
+            const file = response.data;
+            const root = parse(file);
+            const metaTags = root.querySelectorAll('meta');
+            metaTags.forEach(meta => {
+                let property = meta.getAttribute('property');
+                if (property==='og:video:url') {
+                    downloadFile(meta.getAttribute('content'), false)
+                }
+            });
+        }).catch(error => {console.error('Error retrieving HTML:', error)});
+    } else {
+        const { data, headers } = await Axios({url: url, method: 'GET', responseType: 'stream'});
+        data.pipe(fs.createWriteStream('Clip.mp4'));
+    }
 }
 
 async function download(downloadUrl) {
-    const data = getData(downloadUrl)
     if (String(downloadUrl).includes('cdn.medal.tv/ugcc/content-social')) {
-        const stream = fs.createWriteStream('Clip.mp4');
-        data.pipe(stream);
-    } else {
-        
-    }
+        downloadFile(downloadUrl, false)
+    } else {downloadFile(downloadUrl, true)}
 
 }
 download('https://medal.tv/games/roblox/clips/1hxoTxj8oGAtWD/jgrKC05WD8iT')
